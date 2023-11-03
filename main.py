6e0ccc6475d4b733e2212a1e47d118f467316e7f
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2023-11-03 22:49:38 +0900

    Revert "수정 중"
    This reverts commit f41b29ffeed2f4177ef44ade9e920722d6653a71.

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 
+# Initialize LangChain's ChatOpenAI model
 chat_model = ChatOpenAI()
 
 st.title("Game Character Generator")
 
-character_background = st.text_area("Enter character background")
-character_personality = st.text_area("Enter character personality")
-character_class = st.selectbox("Select Character Class", ["Warrior", "Mage", "Rogue"])
+# Upload character image (not used in this example)
+uploaded_file = st.file_uploader("Upload Character Image")
 
+# Input for character description
+character_description = st.text_area("Enter character background and traits")
+
+# When the button is pressed, process the description and output game stats
 if st.button("Generate Stats"):
-    with st.spinner('캐릭터 분석중...'):
-        if character_background and character_personality and character_class:
-            character_description = f"Background: {character_background}, Personality: {character_personality}. Class: {character_class}"
+    if character_description:
+        # Construct a prompt for LangChain
+        prompt = f"Based on the character description: {character_description}, calculate game stats."
+        
+        # Assuming there's a method called 'predict' in ChatOpenAI (replace 'predict' with the correct method name)
+        result = chat_model.predict(prompt)  # Modify this line
+        
+        # Parse the result to extract game stats (modify this part based on the actual structure of 'result')
+        stats_str = result['message']['content']  # Adjust this line
+        stats_list = stats_str.split(', ')
+        stats_dict = {stat.split(': ')[0]: int(stat.split(': ')[1]) for stat in stats_list}
+        
+        # Output the stats
+        st.write(stats_dict)
+    else:
+        st.write("Please enter a character description.")
 
-            character_info = ("해당 캐릭터의 배경은 다음과 같습니다 : " + character_background +
-                                      "해당 캐릭터의 성격은 다음과 같습니다 : " + character_personality +
-                                      "해당 캐릭터의 직업은 다음과 같습니다 : " + character_class)
-            response = chat_model.predict("해당 캐릭터의 정보를 가지고 캐릭터에 어울리는 공격력, 방어력, 이동속도를 추천해주세요. 정보는 다음과 같습니다 : " + character_info)
-            st.write(response)
-       
+# Optionally, consider other input fields for additional parameters
+character_class = st.selectbox("Select Character Class", ["Warrior", "Mage", "Rogue"])
