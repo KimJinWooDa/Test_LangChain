429d7cb4c5959d06cef1a5981c11e042aa417e9b
Jin <76438011+KimJinWooDa@users.noreply.github.com>
2023-11-03 19:55:27 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-#from dotenv import load_dotenv
-#load_dotenv()
-from langchain.chat_models import ChatOpenAI
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
 
-
+# Initialize LangChain's ChatOpenAI model
 chat_model = ChatOpenAI()
 
-st.title('인공지능 시인')
+st.title("Game Character Generator")
 
-title = st.text_input('주제를 쓰세요')
+# Upload character image (not used in this example)
+uploaded_file = st.file_uploader("Upload Character Image")
 
-if st.button('만들기 버튼'):   
-    with st.spinner('시 작성중...'):
-        result = chat_model.predict(title + "에 대한 시를 써줘!")
-        st.write('시의 내용은', result)
+# Input for character description
+character_description = st.text_area("Enter character background and traits")
 
+# When the button is pressed, process the description and output game stats
+if st.button("Generate Stats"):
+    if character_description:
+        # Construct a prompt for LangChain
+        prompt = f"Based on the character description: {character_description}, calculate game stats."
+        
+        # Get a response from LangChain
+        result = chat_model.ask(prompt)
+        
+        # Assume the result contains the stats in a string like "Attack: 10, Defense: 10, Speed: 10"
+        stats_str = result['message']['content']  # Adjust this line based on the actual structure of 'result'
+        stats_list = stats_str.split(', ')
+        stats_dict = {stat.split(': ')[0]: int(stat.split(': ')[1]) for stat in stats_list}
+        
+        # Output the stats
+        st.write(stats_dict)
+    else:
+        st.write("Please enter a character description.")
 
-
+# Optionally, consider other input fields for additional parameters
+character_class = st.selectbox("Select Character Class", ["Warrior", "Mage", "Rogue"])
